61b6225fc963d59586e91652b8f0d4d318e85f7f
Kendall Bennett <kendallb@amain.com>
2021-02-22 14:07:10 -0800

    Saga demo

---
+++ MySql/Sagas/Common/CommonRebusConfigurationExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System.Configuration;
+using Rebus.Config;
+using Serilog;
+// ReSharper disable ArgumentsStyleLiteral
+
+namespace Common
+{
+    public static class CommonRebusConfigurationExtensions
+    {
+        public static RebusConfigurer ConfigureEndpoint(this RebusConfigurer configurer, EndpointRole role)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["RebusDatabase"]?.ConnectionString ?? throw new ConfigurationErrorsException("Could not find 'RebusDatabase' connection string");
+
+            configurer
+                .Logging(l => l.Serilog(Log.Logger))
+                .Transport(t =>
+                {
+                    if (role == EndpointRole.Client)
+                    {
+                        t.UseMySqlAsOneWayClient(new MySqlTransportOptions(connectionString));
+                    }
+                    else
+                    {
+                        t.UseMySql(new MySqlTransportOptions(connectionString), Config.AppSetting("QueueName"));
+                    }
+                })
+                .Subscriptions(s =>
+                {
+                    var subscriptionsTableName = Config.AppSetting("SubscriptionsTableName");
+
+                    s.StoreInMySql(connectionString, subscriptionsTableName, isCentralized: true);
+                })
+                .Sagas(s =>
+                {
+                    if (role != EndpointRole.SagaHost) return;
+
+                    var dataTableName = Config.AppSetting("SagaDataTableName");
+                    var indexTableName = Config.AppSetting("SagaIndexTableName");
+
+                    // store sagas in SQL Server to make them persistent and survive restarts
+                    s.StoreInMySql(connectionString, dataTableName, indexTableName);
+                });
+
+            return configurer;
+        }
+    }
+}
---
+++ MySql/Sagas/SagaDemo/Installers/RebusHandlersInstaller.cs
@@ -0,0 +1,17 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Rebus.CastleWindsor;
+using SagaDemo.Handlers;
+
+namespace SagaDemo.Installers
+{
+    public class RebusHandlersInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            // just register all Rebus handlers from this assembly
+            container.RegisterHandler<PayoutSaga>();
+        }
+    }
+}
